e8c4a81e9f60b0118d36f196c1e7c639d2b72611
Thibault231 <salgues231@gmail.com>
2020-04-09 14:44:09 +0200

    add move 1

--- fbapp/run.py
+++ fbapp/run.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from fbapp import app
+app.config.from_object('config')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- fbapp/views.py
+++ fbapp/views.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Config options - Make sure you created a 'config.py' file.
-#app.config.from_object('config')
+app.config.from_object('config')
 
 @app.route('/')
 def index():
